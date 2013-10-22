565a6042b3931e39b6b68b2e349fe643dcf1f8cc
Sony Theakanath <Sony@Sonys-MacBook-Pro.local>
2013-10-22 15:00:56 -0700

    Initial Commit

---
+++ Carillon Lite/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Carillon Lite
+//
+//  Created by Sony Theakanath on 10/22/13.
+//  Copyright (c) 2013 Sony Theakanath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Carillon Lite/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Carillon Lite
+//
+//  Created by Sony Theakanath on 10/22/13.
+//  Copyright (c) 2013 Sony Theakanath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
